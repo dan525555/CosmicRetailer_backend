96a2696f2dd43d3131ef1d9d3247aac8a2b02403
LogicalInsanity <idkwtw223@gmail.com>
2023-10-22 18:29:48 +0200

    login start

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+import json  # rationale: for reading config.json file
+import pymongo  # rationale:
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (("http://localhost:5173", "http://localhost:5000"),)
+app.config.from_file("config.json", load=json.load)
+mongo_client = pymongo.MongoClient(app.config["MONGO_API"])
+users_db = mongo_client["data"]["users"]
+
+# rationale for that kind of imports:
+# https://stackoverflow.com/questions/11994325/how-to-divide-flask-app-into-multiple-py-files
+import log_in as log_in
---
+++ log_in.py
@@ -0,0 +1,76 @@
+from app import app, users_db
+from flask import request, jsonify
+import flask_login as fl
+from bson.objectid import ObjectId
+import re  # regex
+
+# login part
+login_manager = fl.LoginManager()
+login_manager.init_app(app)
+logged_users = set()
+
+
+# user class
+class User(fl.UserMixin):
+    def __init__(self, name) -> None:
+        super().__init__()
+        self.id = name
+
+
+@login_manager.user_loader
+def load_user(id):
+    id = ObjectId(str(id))
+    for user in logged_users:
+        if user.id == id:
+            return user
+    return None
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form["name"]
+    password = request.form["password"]
+    x = users_db.find_one({"name": name})
+    if password == x["password"]:
+        user = User(x["_id"])
+        fl.login_user(user)
+        logged_users.add(user)
+        return "Success", 200
+    return jsonify({"message": "Incorrect password or login", "code": 418})
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form["email"]
+    name = request.form["name"]
+    password = request.form["password"]
+
+    if users_db.find_one({"name": name}) != None:
+        return jsonify({"message": "This nickname is already in use", "code": 418})
+    if not re.fullmatch(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", email):
+        return jsonify({"message": "This is not a proper email addess", "code": 418})
+    if users_db.find_one({"email": email}) != None:
+        return jsonify({"message": "This email is already in use", "code": 418})
+
+    users_db.insert_one(
+        {
+            "email": email,
+            "name": name,
+            "password": password,
+            "admin": False,
+        }
+    )
+    x = users_db.find_one({"name": name})
+    logs_db.insert_one({"_id": x["logs_id"]})
+    user = User(x["_id"])
+    fl.login_user(user)
+    logged_users.add(user)
+    return "Success", 200
+
+
+@app.route("/logout", methods=["POST"])
+@fl.login_required
+def logout():
+    logged_users.remove(fl.current_user)
+    fl.logout_user()
+    return "Success", 200
