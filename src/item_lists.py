52c4117de78026b9cb8de9d3bfc8253ebc85e1a4
cpprian <cpprian456@gmail.com>
2023-10-26 11:29:52 +0200

    refactor item_lists

--- src/item_lists.py
+++ src/item_lists.py
@@ -1,21 +1,26 @@
 from app import app, users_db
 from flask import request, jsonify
-from bson.objectid import ObjectId
-from flask_jwt import jwt_required, current_identity  # Import JWT
+from jwt import jwt_required
+from bson import ObjectId
+
+def get_paginated_items(item_list, page, per_page):
+    start = (page - 1) * per_page
+    end = start + per_page
+    return item_list[start:end]
 
 # Define an endpoint for displaying a specific user's items
 @app.route("/user_items/<user_id>", methods=["GET"])
+@jwt_required()
 def get_specific_user_items(user_id):
     page = int(request.args.get('page', 1))
     per_page = int(request.args.get('per_page', 10))
-    start = (page - 1) * per_page
-    end = start + per_page
 
     user = users_db.find_one({"_id": ObjectId(user_id)})
 
     if user:
         user_items = user.get('items', [])
-        return jsonify({"items": user_items[start:end], "message": "Success", "code": 200})
+        paginated_items = get_paginated_items(user_items, page, per_page)
+        return jsonify({"items": paginated_items, "message": "Success", "code": 200})
     else:
         return jsonify({"message": "User not found", "code": 404})
 
@@ -25,8 +30,6 @@ def get_specific_user_items(user_id):
 def get_all_items():
     page = int(request.args.get('page', 1))
     per_page = int(request.args.get('per_page', 10))
-    start = (page - 1) * per_page
-    end = start + per_page  
 
     all_items = []
 
@@ -34,6 +37,6 @@ def get_all_items():
         user_items = user.get('items', [])
         all_items.extend(user_items)
 
-    paginated_items = all_items[start:end]
+    paginated_items = get_paginated_items(all_items, page, per_page)
 
     return jsonify({"items": paginated_items, "message": "Success", "code": 200})
