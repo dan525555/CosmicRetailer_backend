dd217de4b71926bc0f00c9417ad3bf7b96ec7bb3
cpprian <cpprian456@gmail.com>
2023-11-11 13:50:03 +0100

    turn off pagination from item lists

--- src/item_lists.py
+++ src/item_lists.py
@@ -6,28 +6,40 @@ from flask_jwt_extended import jwt_required
 from bson import ObjectId
 
 
-def get_paginated_items(item_list, page, per_page):
-    start = (page - 1) * per_page
-    end = start + per_page
-    return item_list[start:end]
+# def get_paginated_items(item_list, page, per_page):
+#     start = (page - 1) * per_page
+#     end = start + per_page
+#     return item_list[start:end]
 
 
 # Define an endpoint for displaying a specific user's items
 @app.route("/user_items/<user_id>", methods=["GET"])
 @jwt_required()
 def get_specific_user_items(user_id):
-    page = int(request.args.get("page", 1))
-    per_page = int(request.args.get("per_page", 10))
+    # page = int(request.args.get("page", 1))
+    # per_page = int(request.args.get("per_page", 10))
+
+    # user = users_db.find_one({"_id": ObjectId(user_id)})
+
+    # if user:
+    #     user_items = user.get("items", [])
+    #     paginated_items = get_paginated_items(user_items, page, per_page)
+    #     paginated_items_json_serializable = json.loads(json.dumps(paginated_items, default=convert_to_json_serializable))
+
+    #     return jsonify(
+    #         {"items": paginated_items_json_serializable, "message": "Success", "code": 200}
+    #     )
+    # else:
+    #     return jsonify({"message": "User not found", "code": 404})
 
     user = users_db.find_one({"_id": ObjectId(user_id)})
 
     if user:
         user_items = user.get("items", [])
-        paginated_items = get_paginated_items(user_items, page, per_page)
-        paginated_items_json_serializable = json.loads(json.dumps(paginated_items, default=convert_to_json_serializable))
+        user_items_serializable = json.loads(json.dumps(user_items, default=convert_to_json_serializable))
 
         return jsonify(
-            {"items": paginated_items_json_serializable, "message": "Success", "code": 200}
+            {"items": user_items_serializable, "message": "Success", "code": 200}
         )
     else:
         return jsonify({"message": "User not found", "code": 404})
@@ -37,16 +49,23 @@ def get_specific_user_items(user_id):
 @app.route("/all_items", methods=["GET"])
 @jwt_required()
 def get_all_items():
-    page = int(request.args.get("page", 1))
-    per_page = int(request.args.get("per_page", 10))
+    # page = int(request.args.get("page", 1))
+    # per_page = int(request.args.get("per_page", 10))
 
-    skip = (page - 1) * per_page
+    # skip = (page - 1) * per_page
 
-    paginated_items_cursor = items_db.find().skip(skip).limit(per_page)
-    paginated_items = list(paginated_items_cursor)
+    # paginated_items_cursor = items_db.find().skip(skip).limit(per_page)
+    # paginated_items = list(paginated_items_cursor)
 
-    paginated_items_json_serializable = json.loads(json.dumps(paginated_items, default=convert_to_json_serializable))
+    # paginated_items_json_serializable = json.loads(json.dumps(paginated_items, default=convert_to_json_serializable))
+
+    # return jsonify(
+    #     {"items": paginated_items_json_serializable, "message": "Success", "code": 200}
+    # )
+
+    items = items_db.find()
+    items_serializable = json.loads(json.dumps(items, default=convert_to_json_serializable))
 
     return jsonify(
-        {"items": paginated_items_json_serializable, "message": "Success", "code": 200}
+        {"items": items_serializable, "message": "Success", "code": 200}
     )
